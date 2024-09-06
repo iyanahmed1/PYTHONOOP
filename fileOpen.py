469712742e9ad021c94f1ee8603e9dc5b29c25aa
iyanahmed1 <malumasaid123@gmail.com>
2024-09-06 19:01:58 +0300

    commit

--- fileOpen.py
+++ fileOpen.py
@@ -1,5 +1,19 @@
-fh=open('poem.txt','rt',encoding='UTF8')
+"""fh=open('poem.txt','rt',encoding='UTF8')
 
 for line in fh.readlines():
     print(line.strip())#Removes the trailing and preceding white scapes in the lines 
 fh.close()
+
+#delete the file
+#we use the os.remove()
+#import the os module
+import os
+try(
+    if os.path.exists('peom.txt'):
+        os.remove('C:/Users/Admin/Documents/Github/PYTHONOOP/poem.txt')
+
+        print('File deleted sucessfully')
+
+    else:
+    print('File not found')
+)""""
